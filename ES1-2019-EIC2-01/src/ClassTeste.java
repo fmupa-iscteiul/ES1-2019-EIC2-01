b8c6f39dd6a8ecde9e5d5f44ce17edb4bd471afa
fmupa-iscteiul <fmupa@iscte-iul.pt>
2019-10-03 16:44:28 +0100

    commit com push

--- ES1-2019-EIC2-01/src/ClassTeste.java
+++ ES1-2019-EIC2-01/src/ClassTeste.java
@@ -4,6 +4,7 @@ public class ClassTeste {
 	public static void main(String[] args) {
 		System.out.println("Esta é uma classe teste");
 		System.out.println("teste1");
+		System.out.println("fazer commit and push");
 	}
 
 }
