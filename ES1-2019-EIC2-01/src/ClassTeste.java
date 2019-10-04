01d8b38e0780c843ce4fe8bf9bd68b0d6b2c1908
fmupa-iscteiul <fmupa@iscte-iul.pt>
2019-10-04 15:44:03 +0100

    commit

--- ES1-2019-EIC2-01/src/ClassTeste.java
+++ ES1-2019-EIC2-01/src/ClassTeste.java
@@ -14,7 +14,7 @@ public class ClassTeste {
 		System.out.println("algo");
 		System.out.println("Pois");
 		System.out.println("Pois claro");
-		System.out.println("isto não dá");
+		System.out.println("isto não dá para fazer pussh");
 	}
 
 }
