d343bf0d8faf0fdb712687cc9ad30673347f5da5
fmupa-iscteiul <fmupa@iscte-iul.pt>
2019-10-04 13:39:32 +0100

    não fez segundo ramo

--- ES1-2019-EIC2-01/src/Classe2.java
+++ ES1-2019-EIC2-01/src/Classe2.java
@@ -3,6 +3,7 @@ public class Classe2 {
 	public static void main(String[] args) {
 		System.out.println("nova classe");
 		System.out.println("foi feito um novo branch");
+		System.out.println("está tudo igual");
 	}
 
 }
