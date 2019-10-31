af81c5b705e3dadbf3d1234984e123f65cd89261
bmapa-iscteiul <bmapa@iscte-iul.pt>
2019-10-31 12:07:15 +0000

    Criação das 3 tabs

---
+++ ES1-2019-EIC2-01/src/Avaliar_Defeitos.java
@@ -0,0 +1,8 @@
+
+public class Avaliar_Defeitos {
+
+	public Avaliar_Defeitos() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ ES1-2019-EIC2-01/src/Criar_Regra.java
@@ -0,0 +1,8 @@
+
+public class Criar_Regra {
+
+	public Criar_Regra() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
