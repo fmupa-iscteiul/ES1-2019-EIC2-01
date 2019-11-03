1cd30655dfe2b358179cf7db921d32d5252da1ce
Sene Conté <sceee@iscte-iul.pt>
2019-11-03 16:48:43 +0000

    adição de três botões(File,criar regra e avaliar_defeitos);

--- ES1-2019-EIC2-01/src/App.java
+++ ES1-2019-EIC2-01/src/App.java
@@ -1,8 +1,24 @@
 
+
+
 public class App {
-
+	
 	public App() {
-		// TODO Auto-generated constructor stub
+		File file = new File();
+		Avaliar_Defeitos av = new Avaliar_Defeitos();
+		Criar_Regra c = new Criar_Regra();
+		Window.getInstance().addTagPainel(file.getName(), file.getPanel());
+		Window.getInstance().addTagPainel(av.getName(), av.getPanel());
+		Window.getInstance().addTagPainel(c.getName(), c.getPanel());
 	}
-
+	
+	private void start() {
+		Window.getInstance().open();
+	}
+	
+	
+	 public static void main(String[] args) {
+		App app = new App();
+		app.start();
+		}
 }
