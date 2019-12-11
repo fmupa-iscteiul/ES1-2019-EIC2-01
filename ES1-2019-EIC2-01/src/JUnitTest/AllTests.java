ddec878a22582829c667e0818c0a3001d2bb15a2
pagve <pagve@iscte-iul.pt>
2019-12-11 16:47:03 +0000

    JUnit Criar_Regra

--- ES1-2019-EIC2-01/src/JUnitTest/AllTests.java
+++ ES1-2019-EIC2-01/src/JUnitTest/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.runner.JUnitPlatform;
 
 @RunWith(JUnitPlatform.class)
-@SelectClasses({TestRegra.class,TestJTableSample.class,Test_Leitura_Ficheiro.class, TestAvaliar_Defeitos.class})
+@SelectClasses({TestRegra.class,TestJTableSample.class,Test_Leitura_Ficheiro.class, TestAvaliar_Defeitos.class, TestCriar_Regra.class})
 public class AllTests {
 	
 }
